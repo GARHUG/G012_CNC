5f09797f75a0c2b813d9a9f27261f065556f7749
GARHUG <apytokypo0608@gmail.com>
2024-04-22 20:46:36 +0900

    変更

--- src/parser.py
+++ src/parser.py
@@ -1,7 +1,14 @@
-class Reader:
-    ...
+from state import State
 
+
+class Reader:
+    def __init__(self, parser):
+        self.parser = parser
 
 class Parser:
-    def __init__(self):
-        self.reader = Reader()
+    def __init__(self, state: State):
+        self.state = state
+        self.reader = Reader(self.state)
+
+    def cycle_start(self):
+        ...
